ae9c693079c6fdb74c64e0ef8715808b1012a992
Anish Kumar <123398048+anish-kumarbasti@users.noreply.github.com>
2023-08-10 15:22:41 +0530

    spatie project first uploading

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,23 @@
+<x-admin-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Admin') }}
+        </h2>
+    </x-slot>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-4">
+                <h1>Sidebar</h1>
+            </div>
+        </div>
+    </div>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    {{ __("You're logged in!") }}
+                </div>
+            </div>
+        </div>
+    </div>
+</x-admin-layout>
